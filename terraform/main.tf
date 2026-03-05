641635a22716e1eae8a39650c5627b357ffb02a3
Ayush Dubey <ayush.dubey@bellblazetech.com>
2026-03-05 13:03:37 +0530

    Finally

--- terraform/main.tf
+++ terraform/main.tf
@@ -91,7 +91,7 @@ resource "aws_instance" "app_server" {
   instance_type               = var.instance_type
   key_name                    = var.key_pair_name
  # vpc_security_group_ids      = [aws_security_group.app_sg.id]
-  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
+ # iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
   associate_public_ip_address = true
 
   user_data = <<-EOF
